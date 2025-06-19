18f26f045c5cd30ed15ba5b58eb791a0fa35622e
Rokas Braidokas <rokasbraidokas@gmail.com>
2025-06-19 15:24:49 +0300

    Added response types

--- src/Controllers/TodoController.cs
+++ src/Controllers/TodoController.cs
@@ -4,12 +4,15 @@ using TodoApi.Dtos;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/todo")]
     [ApiController]
+    [Route("api/todo")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     public class TodoController(ILogger<TodoController> _logger, TodoDbContext _context) : ControllerBase
     {
         // GET: api/todo
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
         {
             _logger.LogInformation("Fetching all todos");
@@ -18,6 +21,8 @@ namespace TodoApi.Controllers
 
         // GET: api/todo/{id}
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Todo>> GetTodo(int id)
         {
             _logger.LogInformation("Fetching todo with ID: {id}", id);
@@ -35,87 +40,113 @@ namespace TodoApi.Controllers
         // PUT: api/todo/{id}
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> PutTodo(int id, UpdateTodoDto todo)
         {
-            _logger.LogInformation("Updating todo with ID: {id}", id);
-            var existingTodo = await _context.Todos.FindAsync(id);
-            if (existingTodo == null)
-            {
-                _logger.LogWarning("Todo with ID: {id} not found for update", id);
-                return NotFound();
-            }
-
-            if (todo.Title is not null) existingTodo.Title = todo.Title;
-            if (todo.Description is not null) existingTodo.Description = todo.Description;
-            if (todo.IsCompleted.HasValue) existingTodo.IsCompleted = todo.IsCompleted.Value;
-            if (todo.DueDate.HasValue) existingTodo.DueDate = todo.DueDate.Value;
-            existingTodo.UpdatedAt = DateTime.UtcNow;
-
             try
             {
-                await _context.SaveChangesAsync();
-                _logger.LogInformation("Todo with ID: {id} updated successfully", id);
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TodoExists(id))
+                _logger.LogInformation("Updating todo with ID: {id}", id);
+                var existingTodo = await _context.Todos.FindAsync(id);
+                if (existingTodo == null)
                 {
-                    _logger.LogWarning("Todo with ID: {id} not found during update", id);
+                    _logger.LogWarning("Todo with ID: {id} not found for update", id);
                     return NotFound();
                 }
-                else
+
+                if (todo.Title is not null) existingTodo.Title = todo.Title;
+                if (todo.Description is not null) existingTodo.Description = todo.Description;
+                if (todo.IsCompleted.HasValue) existingTodo.IsCompleted = todo.IsCompleted.Value;
+                if (todo.DueDate.HasValue) existingTodo.DueDate = todo.DueDate.Value;
+                existingTodo.UpdatedAt = DateTime.UtcNow;
+
+                try
                 {
-                    _logger.LogError("Concurrency error while updating todo with ID: {id}", id);
-                    throw;
+                    await _context.SaveChangesAsync();
+                    _logger.LogInformation("Todo with ID: {id} updated successfully", id);
                 }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!(await (_context.Todos.AnyAsync(e => e.Id == id))))
+                    {
+                        _logger.LogWarning("Todo with ID: {id} not found during update", id);
+                        return NotFound();
+                    }
+                    else
+                    {
+                        _logger.LogError("Concurrency error while updating todo with ID: {id}", id);
+                        throw;
+                    }
+                }
+
+                return NoContent();
             }
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error updating todo with ID: {id}", id);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while updating the todo.");
+            }
         }
-
         // POST: api/todo
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<Todo>> PostTodo(CreateTodoDto todo)
         {
-            Todo newTodo = new Todo
+            try
             {
-                Title = todo.Title,
-                Description = todo.Description,
-                IsCompleted = todo.IsCompleted,
-                DueDate = todo.DueDate,
-                CreatedAt = DateTime.UtcNow,
-                UpdatedAt = DateTime.UtcNow
-            };
-            _context.Todos.Add(newTodo);
-            await _context.SaveChangesAsync();
-            _logger.LogInformation("Created new todo: {@todo}", newTodo);
+                Todo newTodo = new Todo
+                {
+                    Title = todo.Title,
+                    Description = todo.Description,
+                    IsCompleted = todo.IsCompleted,
+                    DueDate = todo.DueDate,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
+                };
+                _context.Todos.Add(newTodo);
+                await _context.SaveChangesAsync();
+                _logger.LogInformation("Created new todo: {@todo}", newTodo);
 
-            return CreatedAtAction("GetTodo", new { id = newTodo.Id }, newTodo);
+                return CreatedAtAction("GetTodo", new { id = newTodo.Id }, newTodo);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error creating new todo");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while creating the todo.");
+            }
         }
 
         // DELETE: api/todo/{id}
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteTodo(int id)
         {
-            _logger.LogInformation("Deleting todo with ID: {id}", id);
-            var todo = await _context.Todos.FindAsync(id);
-            if (todo == null)
+            try
             {
-                _logger.LogWarning("Todo with ID: {id} not found for deletion", id);
-                return NotFound();
+                _logger.LogInformation("Deleting todo with ID: {id}", id);
+                var todo = await _context.Todos.FindAsync(id);
+                if (todo == null)
+                {
+                    _logger.LogWarning("Todo with ID: {id} not found for deletion", id);
+                    return NotFound();
+                }
+
+                _context.Todos.Remove(todo);
+                await _context.SaveChangesAsync();
+                _logger.LogInformation("Todo with ID: {id} deleted successfully", id);
+
+                return NoContent();
             }
-
-            _context.Todos.Remove(todo);
-            await _context.SaveChangesAsync();
-            _logger.LogInformation("Todo with ID: {id} deleted successfully", id);
-
-            return NoContent();
-        }
-
-        private bool TodoExists(int id)
-        {
-            return _context.Todos.Any(e => e.Id == id);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error deleting todo with ID: {id}", id);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while deleting the todo.");
+            }
         }
     }
 }
