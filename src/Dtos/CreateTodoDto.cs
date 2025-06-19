28b66bb927f53ca9c1a98bc9e910111d66247d05
Rokas Braidokas <rokasbraidokas@gmail.com>
2025-06-19 14:27:42 +0300

    Added Todo class and DTOs for Todo

---
+++ src/Dtos/CreateTodoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Dtos
+{
+    public class CreateTodoDto
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required bool IsCompleted { get; set; }
+        public required DateTime DueDate { get; set; }
+    }
+}
---
+++ src/Dtos/UpdateTodoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Dtos
+{
+    public class UpdateTodoDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool? IsCompleted { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
