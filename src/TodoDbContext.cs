fbeebd9232a528c202aada3de8967b77da233d54
Rokas Braidokas <rokasbraidokas@gmail.com>
2025-06-19 14:28:00 +0300

    Created in memory database

---
+++ src/TodoDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi;
+
+public class TodoDbContext(DbContextOptions<TodoDbContext> options) : DbContext(options)
+{
+    public DbSet<Todo> Todos { get; set; }
+}
