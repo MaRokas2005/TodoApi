68f22d987b7b9847b4c0bf671486e6e956c766c7
Rokas Braidokas <rokasbraidokas@gmail.com>
2025-06-26 12:16:04 +0300

    fixed Todo.Service tests

--- tests/Todo.UnitTests/Service/TodoServiceTests.cs
+++ tests/Todo.UnitTests/Service/TodoServiceTests.cs
@@ -5,15 +5,15 @@ using Todo.Persistence.Entities;
 using Todo.Persistence.Interfaces;
 using Todo.Service;
 using Todo.Service.Models;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Todo.UnitTests.Service;
 
 public class TodoServiceTests
 {
-    private Mock<ITodoRepository> _repoMock = null!;
-    private Mock<IMapper> _mapperMock = null!;
-    private TodoService _todoService = null!;
+    private Mock<ITodoRepository> _repoMock;
+    private Mock<IMapper> _mapperMock;
+    private TodoService _todoService;
+
     [SetUp]
     public void Setup()
     {
@@ -23,8 +23,9 @@ public class TodoServiceTests
     }
 
     [Test]
-    public async Task GetAllTodosAsync_ReturnMappedTodos()
+    public async Task GetAllTodosAsync_GetsEntitiesFromRepo_ReturnMappedTodos()
     {
+        // Arrange
         var todos = new List<TodoItem>
         {
             new(1, "TestTodo", "none", false, DateTime.Today.AddDays(2), DateTime.UtcNow, DateTime.UtcNow)
@@ -37,40 +38,50 @@ public class TodoServiceTests
         _repoMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(todos);
         _mapperMock.Setup(mapper => mapper.Map<IEnumerable<TodoDto>>(todos)).Returns(mapped);
 
+        // Act
         var result = await _todoService.GetAllTodosAsync();
 
-        result.ShouldNotBeNull();
+        // Assert
+        _repoMock.Verify(repo => repo.GetAllAsync(), Times.Once);
         result.ShouldBe(mapped);
     }
 
     [Test]
-    public async Task GetTodoByIdAsync_ReturnsMappedTodo_WhenFound()
+    public async Task GetTodoByIdAsync_GetsEntityFromRepo_ReturnsMappedTodo_WhenFound()
     {
+        // Arrange
         var todo = new TodoItem(1, "TestTodo", "none", false, DateTime.Today.AddDays(2), DateTime.UtcNow, DateTime.UtcNow);
         var mapped = new TodoDto(1, "TestTodo", "none", false, DateTime.Today.AddDays(2));
 
         _repoMock.Setup(repo => repo.GetByIdAsync(1)).ReturnsAsync(todo);
         _mapperMock.Setup(mapper => mapper.Map<TodoDto>(todo)).Returns(mapped);
 
+        // Act
         var result = await _todoService.GetTodoByIdAsync(1);
 
-        result.ShouldNotBeNull();
+        // Assert
+        _repoMock.Verify(repo => repo.GetByIdAsync(1), Times.Once);
         result.ShouldBe(mapped);
     }
 
     [Test]
-    public async Task GetTodoByIdAsync_ReturnsMappedTodo_WhenNotFound()
+    public async Task GetTodoByIdAsync_AttemptsToGetEntityFromRepo_ReturnsNull_WhenNotFound()
     {
+        // Arrange
         _repoMock.Setup(repo => repo.GetByIdAsync(1)).ReturnsAsync((TodoItem?)null);
 
+        // Act
         var result = await _todoService.GetTodoByIdAsync(1);
 
+        // Assert
+        _repoMock.Verify(repo => repo.GetByIdAsync(1), Times.Once);
         result.ShouldBeNull();
     }
 
     [Test]
-    public async Task CreateTodoAsync_ReturnsMappedCreatedTodo()
+    public async Task CreateTodoAsync_AddsEntityToRepo_ReturnsMappedCreatedTodo()
     {
+        // Arrange
         var now = DateTime.UtcNow;
         var tomorrow = DateTime.Today.AddDays(1);
         var dto = new CreateTodoDto("Get milk", "desc", false, tomorrow);
@@ -82,14 +93,18 @@ public class TodoServiceTests
         _repoMock.Setup(repo => repo.AddAsync(entity)).ReturnsAsync(created);
         _mapperMock.Setup(mapper => mapper.Map<TodoDto>(created)).Returns(resultDto);
 
+        // Act
         var result = await _todoService.CreateTodoAsync(dto);
 
+        // Assert
+        _repoMock.Verify(repo => repo.AddAsync(entity), Times.Once);
         result.ShouldBe(resultDto);
     }
 
     [Test]
-    public async Task UpdateTodoAsync_ReturnsMappedUpdatedTodo_WhenFound()
+    public async Task UpdateTodoAsync_UpdatesEntityInRepo_ReturnsMappedUpdatedTodo_WhenFound()
     {
+        // Arrange
         var now = DateTime.UtcNow;
         var tomorrow = DateTime.Today.AddDays(1);
         var dto = new UpdateTodoDto(1, "Get milk", "desc", false, tomorrow);
@@ -101,14 +116,18 @@ public class TodoServiceTests
         _repoMock.Setup(repo => repo.UpdateAsync(entity)).ReturnsAsync(updated);
         _mapperMock.Setup(mapper => mapper.Map<TodoDto>(updated)).Returns(resultDto);
 
+        // Act
         var result = await _todoService.UpdateTodoAsync(dto);
 
+        // Assert
+        _repoMock.Verify(repo => repo.UpdateAsync(entity), Times.Once);
         result.ShouldBe(resultDto);
     }
 
     [Test]
-    public async Task UpdateTodoAsync_ReturnsMappedUpdatedTodo_WhenNotFound()
+    public async Task UpdateTodoAsync_AttemptsToUpdateEntityInRepo_ReturnsNull_WhenNotFound()
     {
+        // Arrange
         var now = DateTime.UtcNow;
         var tomorrow = DateTime.Today.AddDays(1);
         var dto = new UpdateTodoDto(1, "Get milk", "desc", false, tomorrow);
@@ -117,28 +136,39 @@ public class TodoServiceTests
         _mapperMock.Setup(mapper => mapper.Map<TodoItem>(dto)).Returns(entity);
         _repoMock.Setup(repo => repo.UpdateAsync(entity)).ReturnsAsync((TodoItem?)null);
 
+        // Act
         var result = await _todoService.UpdateTodoAsync(dto);
 
+        // Assert
+        _repoMock.Verify(repo => repo.UpdateAsync(entity), Times.Once);
         result.ShouldBeNull();
     }
 
     [Test]
     public async Task DeleteTodoAsync_ReturnsTrue_WhenFound()
     {
+        // Arrange
         _repoMock.Setup(repo => repo.DeleteAsync(1)).ReturnsAsync(true);
 
+        // Act
         var result = await _todoService.DeleteTodoAsync(1);
 
+        // Assert
+        _repoMock.Verify(repo => repo.DeleteAsync(1), Times.Once);
         result.ShouldBeTrue();
     }
 
     [Test]
     public async Task DeleteTodoAsync_ReturnsFalse_WhenNotFound()
     {
+        // Arrange
         _repoMock.Setup(repo => repo.DeleteAsync(1)).ReturnsAsync(false);
 
+        // Act
         var result = await _todoService.DeleteTodoAsync(1);
 
+        // Assert
+        _repoMock.Verify(repo => repo.DeleteAsync(1), Times.Once);
         result.ShouldBeFalse();
     }
 }
